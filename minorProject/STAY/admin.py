440fa6b629dd2f3e668852a390fa87f8c029311c
[Samir Shrestha] <[linkwithsamir@gmail.com]>
2023-01-07 11:13:42 +0545

    Revert "STAY - Find your place to stay"
    This reverts commit e655dc9e9c82121a52fb41fa8365ac69794c5cac.

--- minorProject/STAY/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import finded_rooms
-from leaflet.admin import LeafletGeoAdmin
-# Register your models here.
-class roomsAdmin(LeafletGeoAdmin):
- list_display=('name','coordinate','room_rate' ,'des','des')    
-admin.site.register(finded_rooms,roomsAdmin)
-
